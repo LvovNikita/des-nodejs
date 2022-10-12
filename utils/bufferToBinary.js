4468d85c27f74fed0ab07083cc6fdff7c69cf135
Lvov Nikita <LvovNikita>
2022-10-12 18:10:43 +0300

    Change README.md (4)

--- utils/bufferToBinary.js
+++ utils/bufferToBinary.js
@@ -3,6 +3,7 @@ const byteToBinary = byte => byte.toString(2).padStart(8, 0)
 module.exports = buffer => Array
     .from(buffer)
     .map(byte => byteToBinary(byte))
+    // FIXME: use reverse + map(digit, power) instead
     .reduce((prev, curr) => prev + curr)
     .split('')
     .map(Number)
