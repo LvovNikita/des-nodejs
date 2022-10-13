c122b692421510be633181977cc60efb9b751929
Lvov Nikita <LvovNikita>
2022-10-13 13:25:08 +0300

    v1.0.0. Working encryption and decryption

---
+++ examples/index.js
@@ -0,0 +1,30 @@
+const fs = require('node:fs') // для шифрования файлов
+const DES = require('../index.js')
+
+// Ключ шифрования/расшифрования:
+const key = 'SECRETK'
+
+// Создание экземпляра:
+const des = new DES(key)
+
+// Шифрование:
+const plaintext = 'hello world'
+des.encrypt(plaintext)
+console.log(des.data)           // buffer
+console.log(des.dataAsString)   // string
+
+// Расшифрование:
+const ciphertext = des.data
+des.decrypt(ciphertext)
+console.log(des.data)           // buffer
+console.log(des.dataAsString)   // string
+
+// Для шифрования файлов:
+const file = fs.readFileSync('./input.txt')
+des.encrypt(file)
+fs.writeFileSync('output', des.data)
+
+// Для расшифрования файлов:
+const file2 = fs.readFileSync('./output')
+des.decrypt(file2)
+fs.writeFileSync('output2.txt', des.data)
